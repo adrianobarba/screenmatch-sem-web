731339e581a1389b216ce750bec8afc4e15f40c9
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-01-09 13:55:26 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/adrianobarbosa/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.adrianobarbosa.screenmatch;
+
+import br.com.adrianobarbosa.screenmatch.model.DadosSerie;
+import br.com.adrianobarbosa.screenmatch.service.ConsumoApi;
+import br.com.adrianobarbosa.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		//System.out.println(json);
+		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
