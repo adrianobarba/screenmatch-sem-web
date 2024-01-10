2d8477c2604b1c720a1f0d12194c7debb7a376ff
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-01-09 21:53:05 -0300

    segundo commit

--- src/main/java/br/com/adrianobarbosa/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/adrianobarbosa/screenmatch/model/DadosTemporada.java
@@ -3,8 +3,8 @@ package br.com.adrianobarbosa.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosSerie(@JsonAlias("Title") String titulo,
-                         @JsonAlias("totalSeasons") Integer totalTemporada,
-                         @JsonAlias("imdbRating") String avaliacao)  {
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 }
---
+++ src/main/java/br/com/adrianobarbosa/screenmatch/principal/Principal.java
@@ -0,0 +1,46 @@
+package br.com.adrianobarbosa.screenmatch.principal;
+
+import br.com.adrianobarbosa.screenmatch.model.DadosEpisodio;
+import br.com.adrianobarbosa.screenmatch.model.DadosSerie;
+import br.com.adrianobarbosa.screenmatch.model.DadosTemporada;
+import br.com.adrianobarbosa.screenmatch.service.ConsumoApi;
+import br.com.adrianobarbosa.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i<=dados.totalTemporada(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+        for (int i = 0; i < dados.totalTemporada(); i++) {
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
+
+    }
+}
